2835317cdd0acd51ccb0aabef4fd6af4ba4c328d
kaif225 <100071569+kaif225@users.noreply.github.com>
2024-01-12 14:52:53 +0530

    Update ec2-output.tf

--- ec2-output.tf
+++ ec2-output.tf
@@ -13,7 +13,7 @@ output "ec2_public_ip" {
 
 # PRIVATE INSTANCE OUTPUTS
 
-
+/*
 output "ec2_private_id" {
   description = "The ID of the instance"
   value       = module.ec2-private.id
@@ -22,6 +22,17 @@ output "ec2_private_id" {
 output "ec2_private_ip" {
   description = "The public IP address assigned to the instance, if applicable. NOTE: If you are using an aws_eip with your instance, you should refer to the EIP's address directly and not use `public_ip` as this field will change after the EIP is attached"
   value       = module.ec2-private.private_ip
+}
+*/
+output "ec2_private_instance_ids" {
+  description = "List of IDs of instances"
+  value = [for ec2private in module.ec2-private: ec2private.id ]   
+}
+
+## ec2_private_ip
+output "ec2_private_ip" {
+  description = "List of private IP addresses assigned to the instances"
+  value = [for ec2private in module.ec2-private: ec2private.private_ip ]  
 }
 
 
